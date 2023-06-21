e331492e0ac7264ad73d8c5fe4defea567a1f836
CrediBook <credibook@CrediBooks-MacBook-Air.local>
2023-06-21 18:19:04 +0700

    react: adding tic tac toe

---
+++ react/native/src/app.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export default function Game() {
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const currentSquare = history[history.length - 1];
+
+	let squareRows = [];
+	let boardRows = [];
+
+	for (let i = 0; i < currentSquare.length; i++) {
+		squareRows.push(currentSquare[i]);
+		if ((i + 1) % 3 === 0) {
+			const squares = squareRows.map((square, index) => {
+				return (
+					<div className="square" key={"sqaure" + index}>
+						{square}
+					</div>
+				);
+			});
+			const boardRow = <div className="board-row">{squares}</div>;
+			squareRows = [];
+			boardRows.push(boardRow);
+		}
+	}
+
+	return (
+		<>
+			<div className="game">
+				<div className="game-board">
+					<div className="status">Next Player: X</div>
+					{boardRows.map((boardRow) => {
+						{
+							boardRow;
+						}
+					})}
+				</div>
+				<div className="game-info">
+					<ol>
+						<li>
+							<button onClick={() => console.log("button clicked")}>
+								Go to game start
+							</button>
+						</li>
+					</ol>
+				</div>
+			</div>
+		</>
+	);
+}
---
+++ react/next-app/src/app/page.tsx
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <>
+      <button>I&apos;m a button</button>
+    </>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <>
+      <h1 className="color-red">Hello Worlds!</h1>
+      <MyButton></MyButton>
+    </>
+  )
+}
